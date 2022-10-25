8d9a217549baf9ed215d418ce5a1f06ae2355638
Felix Hilgers <felix_hilgers@posteo.de>
2022-10-25 16:47:49 +0200

    feat(masterkey): add create/decrypt/encrypt

--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module github.com/fhilgers/gocryptomator
 
 go 1.18
 
-require github.com/google/go-cmp v0.5.9
+require (
+	github.com/NickBall/go-aes-key-wrap v0.0.0-20170929221519-1c3aa3e4dfc5
+	github.com/google/go-cmp v0.5.9
+	golang.org/x/crypto v0.1.0
+)
+
+require github.com/stretchr/testify v1.8.1 // indirect
--- internal/masterkey/masterkey.go
+++ internal/masterkey/masterkey.go
@@ -1,12 +1,112 @@
 package masterkey
 
-import "io"
+import (
+	"crypto/aes"
+	"crypto/hmac"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/binary"
+	"encoding/json"
+	"io"
+
+	aesWrap "github.com/NickBall/go-aes-key-wrap"
+	"github.com/fhilgers/gocryptomator/internal/constants"
+	"golang.org/x/crypto/scrypt"
+)
 
 type MasterKey struct {
 	EncryptKey []byte
 	MacKey     []byte
 }
 
-func New() (MasterKey, error)
-func (m MasterKey) Marshal(w io.Writer, passphrase string) error
-func Unmarshal(r io.Reader, passphrase string) (MasterKey, error)
+type encryptedMasterKey struct {
+	Version          uint32 `json:"version"`
+	ScryptSalt       []byte `json:"scryptSalt"`
+	ScryptCostParam  int    `json:"scryptCostParam"`
+	ScryptBlockSize  int    `json:"scryptBlockSize"`
+	PrimaryMasterKey []byte `json:"primaryMasterKey"`
+	HmacMasterKey    []byte `json:"hmacMasterKey"`
+	VersionMac       []byte `json:"versionMac"`
+}
+
+func New() (m MasterKey, err error) {
+	m.EncryptKey = make([]byte, constants.MasterEncryptKeySize)
+	m.MacKey = make([]byte, constants.MasterMacKeySize)
+
+	if _, err = rand.Read(m.EncryptKey); err != nil {
+		return
+	}
+
+	_, err = rand.Read(m.MacKey)
+
+	return
+}
+
+func (m MasterKey) Marshal(w io.Writer, passphrase string) (err error) {
+	encKey := encryptedMasterKey{
+		Version:         constants.MasterVersion,
+		ScryptCostParam: constants.MasterScryptCostParam,
+		ScryptBlockSize: constants.MasterScryptBlockSize,
+	}
+
+	encKey.ScryptSalt = make([]byte, constants.MasterScryptSaltSize)
+
+	if _, err = rand.Read(encKey.ScryptSalt); err != nil {
+		return
+	}
+
+	kek, err := scrypt.Key([]byte(passphrase), encKey.ScryptSalt, encKey.ScryptCostParam, encKey.ScryptBlockSize, 1, constants.MasterEncryptKeySize)
+	if err != nil {
+		return
+	}
+
+	cipher, err := aes.NewCipher(kek)
+	if err != nil {
+		return
+	}
+
+	if encKey.PrimaryMasterKey, err = aesWrap.Wrap(cipher, m.EncryptKey); err != nil {
+		return
+	}
+	if encKey.HmacMasterKey, err = aesWrap.Wrap(cipher, m.MacKey); err != nil {
+		return
+	}
+
+	hash := hmac.New(sha256.New, m.MacKey)
+	if err = binary.Write(hash, binary.BigEndian, encKey.Version); err != nil {
+		return
+	}
+
+	encKey.VersionMac = hash.Sum(nil)
+
+	err = json.NewEncoder(w).Encode(encKey)
+
+	return
+}
+
+func Unmarshal(r io.Reader, passphrase string) (m MasterKey, err error) {
+	encKey := &encryptedMasterKey{}
+
+	if err = json.NewDecoder(r).Decode(encKey); err != nil {
+		return
+	}
+
+	kek, err := scrypt.Key([]byte(passphrase), encKey.ScryptSalt, encKey.ScryptCostParam, encKey.ScryptBlockSize, 1, constants.MasterEncryptKeySize)
+	if err != nil {
+		return
+	}
+
+	cipher, err := aes.NewCipher(kek)
+	if err != nil {
+		return
+	}
+
+	if m.EncryptKey, err = aesWrap.Unwrap(cipher, encKey.PrimaryMasterKey); err != nil {
+		return
+	}
+	if m.MacKey, err = aesWrap.Unwrap(cipher, encKey.HmacMasterKey); err != nil {
+		return
+	}
+
+	return
+}
