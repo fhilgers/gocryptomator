4ceedada99af851df79d7ffca1b36fb6ee40f9fd
Felix Hilgers <felix_hilgers@posteo.de>
2023-04-18 00:43:16 +0200

    refactor(pkg/fs): reduce interface

--- pkg/fs/fs.go
+++ pkg/fs/fs.go
@@ -4,15 +4,6 @@ import (
 	"io"
 )
 
-
-type File interface {
-  io.Closer
-  io.Reader
-
-  Readdir(count int) ([]string, error)
-}
-
 type Fs interface {
-  Open(name string) (File, error)
-  Remove(name string) error
+  Open(name string) (io.Reader, error)
 }
